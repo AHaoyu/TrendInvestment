fa1947ad4575559cf997d0a8e05a3faf3a6cb68a
Haoyux <aihaoyu2014@163.com>
2020-05-10 00:02:25 +0800

    add hystrix function

--- index-gather-store-service-module/src/main/java/cn/haoyu/trend/service/IndexService.java
+++ index-gather-store-service-module/src/main/java/cn/haoyu/trend/service/IndexService.java
@@ -1,6 +1,8 @@
 package cn.haoyu.trend.service;
 
 import cn.haoyu.trend.pojo.Index;
+import cn.hutool.core.collection.CollectionUtil;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +16,7 @@ public class IndexService {
     private List<Index> indexes;
     @Autowired RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "third_party_not_connected")
     public List<Index> fetch_indexes_from_third_party(){
         List<Map> temp = restTemplate.getForObject("http://127.0.0.1:8090/indexes/codes.json",List.class);
         return map2Index(temp);
@@ -30,5 +33,13 @@ public class IndexService {
             indexes.add(index);
         }
         return indexes;
+    }
+
+    public List<Index> third_party_not_connected(){
+        System.out.println("third_part_not_connected()");
+        Index index= new Index();
+        index.setCode("000000");
+        index.setName("无效指数代码");
+        return CollectionUtil.toList(index);
     }
 }
